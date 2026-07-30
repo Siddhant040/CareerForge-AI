f9aa7a47b52d644e423653d01038061dd02e3f05
Shanu Jadoun <shanujadoun604@gmail.com>
2026-07-30 18:44:51 +0530

    fix(healthCheck): fix healthCheck modules

--- Backend/src/modules/healthCheck/healthCheck.controller.js
+++ Backend/src/modules/healthCheck/healthCheck.controller.js
@@ -2,7 +2,7 @@
 import { apiResponse } from "../../utils/Api-Response.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
-export const healthCheck = asyncHandler(async (req, res, next) => {
+export const healthCheck = asyncHandler(async (req, res ) => {
     res
     .status(200)
     .json(new apiResponse(200,null, "server is running"));
